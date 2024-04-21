62deb6e01379d15c9ac6e785ad1e8480d7d6e58d
Norphel <113838216+norphel@users.noreply.github.com>
2024-04-21 16:39:12 +0530

    fix mouse-icon position

--- src/components/ui/mouse-scroll.jsx
+++ src/components/ui/mouse-scroll.jsx
@@ -1,7 +1,7 @@
 const mouseScroll = () => {
   return (
-    <div className="absolute bottom-4 left-2/4">
-      <div className="w-[10px] px-[10px] py-[15px] h-[20px] border-2 border-white rounded-[25px] opacity-75 box-content flex justify-center">
+    <div className="absolute bottom-2 left-2/4 -translate-x-2/4">
+      <div className="w-[10px] px-[10px] py-[15px] h-[15px] md:h-[20px] border-2 border-white rounded-[25px] opacity-75 box-content flex justify-center">
         <div className="w-[5px] h-[10px] rounded-[25%] bg-white animate-scroll"></div>
       </div>
     </div>

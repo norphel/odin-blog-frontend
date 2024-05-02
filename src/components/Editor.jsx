a5157c4038c6102b622cf8a07f7268d9f2eb0949
Norphel <113838216+norphel@users.noreply.github.com>
2024-05-02 10:56:05 +0530

    add ui and functionality to create new post

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useEffect, useState } from "react";
 import EditorJS from "@editorjs/editorjs";
 import Header from "@editorjs/header";
 import SimpleImage from "@editorjs/simple-image";
@@ -7,17 +7,12 @@ import NestedList from "@editorjs/nested-list";
 import LinkTool from "@editorjs/link";
 import Embed from "@editorjs/embed";
 import Heading from "./Header";
+import { FormError } from "./ui/form-error";
+import { FormSuccess } from "./ui/form-success";
 
 const DEFAULT_INITIAL_DATA = {
   time: new Date().getTime(),
   blocks: [
-    {
-      type: "header",
-      data: {
-        text: "Title",
-        level: 1,
-      },
-    },
     {
       type: "paragraph",
       data: {
@@ -28,6 +23,11 @@ const DEFAULT_INITIAL_DATA = {
 };
 
 const Editor = () => {
+  const [title, setTitle] = useState("");
+  const [thumbnailImage, setThumbnailImage] = useState(null);
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [successMessage, setSuccessMessage] = useState(null);
+  const [isPending, setPending] = useState(false);
   const ejsInstance = useRef();
 
   const initEditor = () => {
@@ -89,21 +89,76 @@ const Editor = () => {
   }, []);
 
   const handleSave = async () => {
-    if (ejsInstance.current) {
-      const data = await ejsInstance.current.save();
-      console.log(data);
+    setErrorMessage(null);
+    setSuccessMessage(null);
+    setPending(true);
+
+    try {
+      let formData = new FormData();
+
+      if (title === "") throw new Error("Title of post is required!");
+      formData.append("title", title);
+
+      if (thumbnailImage === null)
+        throw new Error("Please provide a thumbnail image");
+      formData.append("thumbnailImage", thumbnailImage);
+
+      if (ejsInstance.current) {
+        const data = await ejsInstance.current.save();
+        console.log(data);
+        formData.append("content", JSON.stringify(data));
+      }
+
+      const response = await fetch("http://localhost:3000/api/v1/posts", {
+        method: "POST",
+        body: formData,
+        credentials: "include",
+      });
+      console.log(response);
+      setSuccessMessage("Saved successfully");
+      setPending(false);
+    } catch (error) {
+      console.log("Failed: ", error);
+      setErrorMessage(`Failed to save! ${error.message}`);
+      setPending(false);
     }
   };
 
   return (
     <div className="mt-2 max-w-screen-xl mx-2 md:mx-4 lg:mx-6 xl:mx-auto min-h-screen">
       <Heading />
-      <div id="editorjs" className="border m-6 md:mx-0 rounded-md "></div>
+      <label htmlFor="title" className="mt-4 mb-2 block font-semibold">
+        Title
+      </label>
+      <input
+        type="text"
+        id="title"
+        name="title"
+        value={title}
+        required
+        onChange={(e) => setTitle(e.target.value)}
+        className="border  w-full p-1 rounded-md"
+      />
+      <label htmlFor="thumbnailImage" className="mt-4 mb-2 block font-semibold">
+        Thumbnail/Cover Image
+      </label>
+      <input
+        type="file"
+        id="thumbnailImage"
+        name="thumbnailImage"
+        required
+        onChange={(e) => setThumbnailImage(e.target.files[0])}
+      />
+      <p className="mt-4 mb-2 font-semibold">Content</p>
+      <div id="editorjs" className="border mx-6 mb-6 md:mx-0 rounded-md "></div>
+
+      <FormError message={errorMessage} />
+      <FormSuccess message={successMessage} />
       <button
         onClick={handleSave}
-        className="mx-6 md:mx-0 px-4 py-1 rounded-xl bg-emerald-100 text-emerald-900 bold"
+        className="mx-6 md:mx-0 my-6 px-4 py-1 rounded-xl bg-emerald-100 text-emerald-900 bold"
       >
-        Save
+        {isPending === true ? "Saving..." : "Save"}
       </button>
     </div>
   );

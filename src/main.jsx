f7188f0f824f84a59d8eb93f0f783a91727fdb38
Norphel <113838216+norphel@users.noreply.github.com>
2024-05-04 23:00:58 +0530

    wrap all router with user context

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { createContext, useState } from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
 import ErrorPage from "./components/ui/error-page.jsx";
@@ -18,64 +19,83 @@ import {
   SignupPage,
 } from "./components/index.js";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />,
-    errorElement: <ErrorPage />,
-    children: [
-      {
-        path: "",
-        element: <LandingPage />,
-      },
-      {
-        path: "articles",
-        element: <AllArticles />,
-      },
-      {
-        path: "articles/:articleID",
-        element: <Article />,
-      },
-      {
-        path: "login",
-        element: <LogInPage />,
-      },
-      {
-        path: "signup",
-        element: <SignupPage />,
-      },
-      {
-        path: "/editor",
-        element: <Editor />,
-      },
-      {
-        path: "dashboard",
-        element: <Dashboard />,
-        children: [
-          {
-            path: "profile",
-            element: <MyProfile />,
-          },
-          {
-            path: "myposts",
-            element: <MyPosts />,
-          },
-          {
-            path: "settings",
-            element: <Settings />,
-          },
-        ],
-      },
-    ],
-  },
-  {
-    path: "/presentation",
-    element: <Presentation />,
-  },
-]);
+export const UserContext = createContext();
+
+const App = () => {
+  const [user, setUser] = useState(() => {
+    const savedUser = localStorage.getItem("user");
+    return savedUser ? JSON.parse(savedUser) : null;
+  });
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home />,
+      errorElement: <ErrorPage />,
+      children: [
+        {
+          path: "",
+          element: <LandingPage />,
+        },
+        {
+          path: "articles",
+          element: <AllArticles />,
+        },
+        {
+          path: "articles/:articleID",
+          element: <Article />,
+        },
+        {
+          path: "login",
+          element: <LogInPage />,
+        },
+        {
+          path: "signup",
+          element: <SignupPage />,
+        },
+        {
+          path: "/editor",
+          element: <Editor />,
+        },
+        {
+          path: "dashboard",
+          element: <Dashboard />,
+          children: [
+            {
+              path: "profile",
+              element: <MyProfile />,
+            },
+            {
+              path: "myposts",
+              element: <MyPosts />,
+            },
+            {
+              path: "settings",
+              element: <Settings />,
+            },
+          ],
+        },
+      ],
+    },
+    {
+      path: "/presentation",
+      element: <Presentation />,
+    },
+    {
+      path: "*",
+      element: <ErrorPage />,
+    },
+  ]);
+
+  return (
+    <UserContext.Provider value={{ user, setUser }}>
+      <RouterProvider router={router} />
+    </UserContext.Provider>
+  );
+};
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>
 );
